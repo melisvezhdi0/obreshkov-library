1c5bef9643d3b36ef875e195c1341bdf797f0b07
Мелис Вежди <M.Vezhdi21g@pmgrz.net>
2026-02-01 01:59:08 +0800

    Initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace ObreshkovLibrary.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}

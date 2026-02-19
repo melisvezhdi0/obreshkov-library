8f828f43b1c3fa2b8aea87ff02e9439aeefd8322
Мелис Вежди <M.Vezhdi21g@pmgrz.net>
2026-02-19 13:26:37 +0200

    Добавен е таг за повече потробности на дадена книга без добавяне на нова категория(затова е махнато 3. поле в Добавяне на категория).

---
+++ Models/BookTitleTag.cs
@@ -0,0 +1,11 @@
+﻿namespace ObreshkovLibrary.Models
+{
+    public class BookTitleTag
+    {
+        public int BookTitleId { get; set; }
+        public BookTitle BookTitle { get; set; } = null!;
+
+        public int TagId { get; set; }
+        public Tag Tag { get; set; } = null!;
+    }
+}
---
+++ Models/Tag.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ObreshkovLibrary.Models
+{
+    public class Tag
+    {
+        public int Id { get; set; }
+
+        [Required, StringLength(50)]
+        public string Name { get; set; } = "";
+
+        public bool IsActive { get; set; } = true;
+
+        public ICollection<BookTitleTag> BookTitleTags { get; set; } = new List<BookTitleTag>();
+    }
+}

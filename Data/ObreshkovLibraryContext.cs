c54dd44cd03fffdb3a132c1ff7de2b9a9224929c
melisvezhdi0 <melisvezhdi@gmail.com>
2026-03-19 12:12:45 +0200

    Add Identity

--- Data/ObreshkovLibraryContext.cs
+++ Data/ObreshkovLibraryContext.cs
@@ -1,9 +1,11 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
 using ObreshkovLibrary.Models;
 
 namespace ObreshkovLibrary.Data
 {
-    public class ObreshkovLibraryContext : DbContext
+    public class ObreshkovLibraryContext : IdentityDbContext<IdentityUser>
     {
         public ObreshkovLibraryContext(DbContextOptions<ObreshkovLibraryContext> options)
             : base(options)
